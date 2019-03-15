f5459c08614e70f69dcec201cade9d9a4c15a78e
Mie Winstrup <mie@septima.dk>
2019-03-15 09:07:16 +0100

    Test af qlr-fil

--- Kortforsyningen/mysettings/settings.py
+++ Kortforsyningen/mysettings/settings.py
@@ -5,8 +5,8 @@ from qgis.PyQt import QtCore
 
 from .qgissettingmanager import *
 #CONFIG_FILE_URL ='https://apps2.kortforsyningen.dk/qgis_knap_config/Kortforsyningen/kf/kortforsyning_data.qlr'
-CONFIG_FILE_URL ='https://labs.septima.dk/qgis-kf-knap/kortforsyning_data_token.qlr'
-
+#CONFIG_FILE_URL ='https://labs.septima.dk/qgis-kf-knap/kortforsyning_data_token.qlr'
+CONFIG_FILE_URL='https://raw.githubusercontent.com/Septima/Qgis_plugin_Kortforsyningen_v3-1/master/kortforsyning_data.qlr'
 class Settings(SettingManager):
     settings_updated = QtCore.pyqtSignal()
 
