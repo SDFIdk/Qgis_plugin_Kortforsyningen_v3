e65fcda80e2f9129decca8c7f9d28f6fff773246
AsgerPetersen <asgerpetersen@gmail.com>
2019-08-22 16:51:58 +0200

    Show link button to settings in messagebar

--- Kortforsyningen/i18n/da.ts
+++ Kortforsyningen/i18n/da.ts
@@ -1,45 +1,43 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
-<context>
-    <name>KfConfig</name>
-    <message>
-        <location filename="kf_config.py" line="140"/>
-        <source>No contact to Kortforsyningen</source>
-        <translation>Ingen kontakt til Kortforsyningen</translation>
-    </message>
-</context>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_DK" sourcelanguage="en">
 <context>
     <name>Kortforsyningen</name>
     <message>
-        <location filename="kortforsyningen.py" line="115"/>
+        <location filename="../kortforsyningen.py" line="123"/>
         <source>Kortforsyningen</source>
         <translation>Kortforsyningen</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="95"/>
-        <source>Check connection and click menu Settings -&gt; Options - &gt; Kortforsyningen -&gt; OK</source>
-        <translation>Tjek forbindelse og klik menu Indstillinger -&gt; Generelle Indstillinger - &gt; Kortforsyningen -&gt; OK</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="96"/>
+        <location filename="../kortforsyningen.py" line="99"/>
         <source>No contact to Kortforsyningen</source>
-        <translation>Ingen kontaskt til Kortforsyningen</translation>
+        <translation>Ingen kontakt til Kortforsyningen</translation>
     </message>
     <message>
-        <location filename="kortforsyningen.py" line="100"/>
-        <source>Token not set or wrong. Select menu Settings -&gt; Options - &gt; Kortforsyningen</source>
-        <translation>Token ikke angivet eller forkert. Klik menu Indstillinger -&gt; Generelle Indstillinger - &gt; Kortforsyningen</translation>
-    </message>
-    <message>
-        <location filename="kortforsyningen.py" line="165"/>
+        <location filename="../kortforsyningen.py" line="181"/>
         <source>About the plugin</source>
         <translation>Om dette plugin</translation>
+    </message>
+    <message>
+        <location filename="../kortforsyningen.py" line="100"/>
+        <source>Check internet connection and Kortforsyningen settings</source>
+        <translation>Kontroller internetforbindelse og indstillinger for Kortforsyningen</translation>
+    </message>
+    <message>
+        <location filename="../kortforsyningen.py" line="106"/>
+        <source>Token not set or wrong</source>
+        <translation>Token er forkert eller ikke angivet</translation>
+    </message>
+    <message>
+        <location filename="../kortforsyningen.py" line="111"/>
+        <source>Open settings</source>
+        <translation>Åbn indstillinger</translation>
     </message>
 </context>
 <context>
     <name>LayerLocatorFilter</name>
     <message>
-        <location filename="layerlocatorfilter.py" line="24"/>
+        <location filename="../layerlocatorfilter.py" line="24"/>
         <source>Kortforsyningen</source>
         <translation>Kortforsyningen</translation>
     </message>
@@ -47,37 +45,37 @@
 <context>
     <name>s</name>
     <message>
-        <location filename="settings.ui" line="14"/>
+        <location filename="../mysettings/settings.ui" line="14"/>
         <source>Kortforsyningen - Settings</source>
         <translation>Kortforsyning - Indstillinger</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="83"/>
+        <location filename="../mysettings/settings.ui" line="92"/>
         <source>Enter token to Kortforsyning</source>
         <translation>Angiv token til Kortforsyning</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="163"/>
+        <location filename="../mysettings/settings.ui" line="124"/>
         <source>Extend Kortforsyningsplugin with qlr-file</source>
         <translation>Udvid Kortforsyningsplugin med qlr-fil</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="79"/>
+        <location filename="../mysettings/settings.ui" line="150"/>
         <source>Select qlr-file</source>
         <translation>Vælg qlr-fil</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="153"/>
+        <location filename="../mysettings/settings.ui" line="160"/>
         <source>Only display backgroundmap from Kortforsyningen</source>
         <translation>Vis kun baggrundskort fra Kortforsyningen</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="83"/>
+        <location filename="../mysettings/settings.ui" line="83"/>
         <source>Token</source>
         <translation>Token</translation>
     </message>
     <message>
-        <location filename="settings.ui" line="99"/>
+        <location filename="../mysettings/settings.ui" line="99"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://kortforsyningen.dk/indhold/min-side-0&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Find your token to Kortforsyningen&lt;/span&gt;&lt;/a&gt; or &lt;a href=&quot;https://kortforsyningen.dk/indhold/min-side-0&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;create a new token&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Not registered as a user at Kortforsyningen? &lt;a href=&quot;http://download.kortforsyningen.dk//content/opret-mig-som-bruger&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Create user &lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;a href=&quot;https://kortforsyningen.dk/indhold/min-side-0&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Find dit token til Kortforsyningen&lt;/span&gt;&lt;/a&gt; eller &lt;a href=&quot;https://kortforsyningen.dk/indhold/min-side-0&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt; opret nyt token&lt;/span&gt;&lt;/a&gt;.&lt;/p&gt;&lt;p&gt;Ikke registreret som bruger på Kortforsyningen? &lt;a href=&quot;http://download.kortforsyningen.dk//content/opret-mig-som-bruger&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;Opret bruger&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
